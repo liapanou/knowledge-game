b587dbfb72afb31f14c0439c85cee2ec6d96e81d
liapanou <liapanou7@gmail.com>
2023-01-26 17:38:47 +0200

    first commit

---
+++ components/Card/index.tsx
@@ -0,0 +1,7 @@
+export function Card() {
+  return (
+    <div className="border border-black rounded-lg ">
+      <div className=" card-back "></div>
+    </div>
+  );
+}
---
+++ components/CardGame/index.tsx
@@ -0,0 +1,24 @@
+import { Card } from "../Card";
+
+export function CardGame() {
+  return (
+    <div className="grid grid-cols-4 gap-2">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
